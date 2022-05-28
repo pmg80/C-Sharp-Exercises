5a74abea8a7b193133ca275ecd13cf89af3b51cd
Pejman <m.pejman80.pm@gmail.com>
2022-05-28 22:11:29 +0430

    Add Exercise4

---
+++ Exercise4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Exercise4
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Enter a word/sentence");
+            string S1 = Console.ReadLine();
+            System.Console.WriteLine("Enter another word/sentence");
+            string S2 = Console.ReadLine();
+
+            for (int i = 0; i < S2.Length; i++)
+            {
+                
+                for(int j=0 ; j < S2.Length ; j++)
+                {
+                    string sub = S2.Substring(i , j+1);
+                            if (S1.IndexOf(sub) != -1)
+                            {
+                                System.Console.WriteLine($"String:{sub}   Index: {S1.IndexOf(sub)}");
+                            }
+                }
+            }
+            
+        }
+
+    }
+}
