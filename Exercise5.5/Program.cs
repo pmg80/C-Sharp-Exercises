5e5417a1798400622fe4c9aafe9274e9035bea41
Pejman <m.pejman80.pm@gmail.com>
2022-05-31 01:13:03 +0430

    exercise 5.5 added

---
+++ Exercise5.5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+
+namespace Exercise5_5
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("Enter your file's path");
+            string FilePath = Console.ReadLine();
+
+            do {
+
+            Console.WriteLine("Enter your files name:");
+            string FileName = Console.ReadLine();
+            
+            string path = FilePath + @"\" + FileName + @".txt";
+            
+            Console.WriteLine("Enter your text");
+            string Text = Console.ReadLine();
+            
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(Text);
+
+            } 
+
+            Console.WriteLine("Do you want to add more text files to this directory?(Yes or no)");
+            
+        
+            } while (Console.ReadLine().ToLower() == "yes");
+        }
+    }
+}
