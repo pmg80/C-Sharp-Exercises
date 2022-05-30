c65342d7bfd1de0f3f643c0656f38a1e8b5e7949
Pejman <m.pejman80.pm@gmail.com>
2022-05-30 20:06:32 +0430

    Exercise 5 added

---
+++ Exercise5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+
+namespace Exercise5
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+           byte Counter = 0;
+
+           Console.WriteLine("Enter a Big Number");
+           string BigNum = Console.ReadLine();
+
+           Console.WriteLine($"Enter a digit");
+           int Digit = Convert.ToInt32(Console.ReadLine());
+
+           for(int i=0 ; i<BigNum.Length ; i++)
+           {
+              int sub = Convert.ToInt32(BigNum.Substring(i , 1));
+              if ( sub == Digit)
+              {
+                  Counter++;
+              }
+           }
+           Console.WriteLine($"Your digit has been repeated {Counter} times");
+           
+
+    }
+}
+}
