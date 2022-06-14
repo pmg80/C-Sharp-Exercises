4a7e9eb3bd6cd04fe7a58c4f988cb7c716fbd810
Pejman <m.pejman80.pm@gmail.com>
2022-06-14 22:36:14 +0430

    recursive exam

---
+++ Exam/Program.cs
@@ -0,0 +1,53 @@
+﻿
+namespace ClassCsharp
+{
+
+
+    class program 
+    {
+        
+        static void Main(string[] args)
+        {
+        System.Console.WriteLine("Enter a word: ");
+        string word = Console.ReadLine();
+        int i = 0;
+        
+        System.Console.WriteLine(Recursive(i , word ));
+
+        }
+        
+        public static bool Recursive(int i , string word )
+        {
+            
+            if (word.Length % 2 == 1)
+            {
+                if (i < word.Length )
+                {
+                    if(word[i]==word[word.Length-1-i])
+                    {
+                        if(word.Length/2 == i )
+                        {
+                            return true;
+                        }
+                        else
+                        {
+                            return Recursive(i+1 , word);
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            } 
+        }
+    }
+}
