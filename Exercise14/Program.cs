f5a3ecd1e39191a00635026f819952c6e63f8d71
pmg80 <m.pejman80.pm@gmail.com>
2022-07-20 17:55:25 +0430

    Equal arrays

---
+++ Exercise14/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise14
+{
+    namespace Exercise14
+    {
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                int[] arr1 = {1 , 5 , 8};
+                int[] arr2 = {1 , 5 , 8};
+                string[] arr3 = {"1" , "5" , "8"};
+                string[] arr4 = {"1" , "5" , "7"};
+                System.Console.WriteLine(IsEqual(arr1 , arr2));
+                System.Console.WriteLine(IsEqual(arr4 , arr3));
+            }
+
+
+            public static bool IsEqual<T>(T[] arr1 , T[] arr2)
+            {
+                if(arr1.SequenceEqual(arr2))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    
+
+    }
+}
